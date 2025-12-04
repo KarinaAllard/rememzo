3497c9825fac9fac9973f9094906c7a589d5c317
Karina Allard <karina.allard.92@gmail.com>
2025-12-04 13:54:43 +0100

    feat: add QuestionsLibrary model

---
+++ backend/src/models/QuestionsLibrary.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IQuestion extends Document {
+    type: string;
+    requiredItemTypes: string[];
+    templateText: string;
+    optionsCount: number;
+    translations?: Record<string, string>;
+}
+
+const QuestionSchema: Schema<IQuestion> = new Schema({
+    type: { type: String, required: true },
+    requiredItemTypes: { type: [String], required: true },
+    templateText: { type: String, required: true },
+    optionsCount: { type: Number, required: true },
+    translations: { type: Map, of: String},
+});
+
+export default mongoose.model<IQuestion>("questionslibrary", QuestionSchema);
