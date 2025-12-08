056929e5b94ede3ba4485d5b4926a853ea0c310d
Karina Allard <karina.allard.92@gmail.com>
2025-12-08 09:50:38 +0100

    feat: add random slot selection service for scene generator

---
+++ backend/src/services/sceneGenerator/slotPicker.ts
@@ -0,0 +1,19 @@
+import { ISceneTemplate } from "../../models/SceneTemplates";
+
+export function pickRandomSlots(template: ISceneTemplate) {
+	const allSlots = [...template.slots];
+
+	const numberOfItems = Math.max(
+		1,
+		Math.floor(Math.random() * template.maxItems) + 1
+	);
+
+	for (let i = allSlots.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[allSlots[i], allSlots[j]] = [allSlots[j], allSlots[i]];
+	}
+
+	const selectedSlots = allSlots.slice(0, numberOfItems);
+
+	return selectedSlots;
+}
