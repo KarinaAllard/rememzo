427625c71cef97222837bf1c9929101fc05796be
Karina Allard <karina.allard.92@gmail.com>
2025-12-10 14:41:58 +0100

    feat: add input submit into button styling, add a simple login form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,18 @@
 export const Login = () => {
     return (
-        <>
-        
-        </>
+        <div className="">
+            <h1 className="text-4xl text-(--text) mb-6">Log in</h1>
+            <form className="flex flex-col">
+                <label className="mb-4">Email
+                    <br/>
+                    <input type="email" name="email" id="email" className="border border-(--text-hover)" />
+                </label>
+                <label className="mb-4">Password
+                    <br/>
+                <input type="password" name="password" id="password" className="border border-(--text-hover)" />
+                </label>
+                <input type="submit" value="Log in" />
+            </form>
+        </div>
     )
 }
