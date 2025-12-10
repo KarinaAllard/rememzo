00de69b1d4fa2a8dc9c3802cb5c44d6061ca578f
Karina Allard <karina.allard.92@gmail.com>
2025-12-10 14:13:41 +0100

    feat: add mobile logo, refactor navbar, add homepage content and btn styling

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,19 @@
-import { Link } from "react-router"
+import { Link, NavLink } from "react-router"
 import logo from "../assets/logo-v5.png"
+import mobileLogo from "../assets/mobile-logo-v5.png"
 
 export const Navbar = () => {
 
     return (
         <header className="p-4 flex justify-between px-10 items-center">
-            <img src={logo} alt="Rememzo" className="w-[300px]" />
+            <Link to="/">
+                <img src={logo} alt="Rememzo" className="hidden md:block w-[300px]" />
+                <img src={mobileLogo} alt="Rememzo" className="block w-[115px] ml-1 md:hidden" />
+            </Link>
             <nav>
                 <ul className="flex gap-6 pt-3">
-                <li><Link to="/" className="hover:text-(--text-hover)">Home</Link></li>
-                <li><Link to="/play" className="hover:text-(--text-hover)">Play</Link></li>
-                <li><Link to="/login" className="hover:text-(--text-hover)">Login</Link></li>
+                <li><NavLink to="/play" className="hover:text-(--text-hover)">Play</NavLink></li>
+                <li><NavLink to="/login" className="hover:text-(--text-hover)">Login</NavLink></li>
                 </ul>
             </nav>
         </header>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router"
+
 export const Home = () => {
     return (
-        <>
-        
-        </>
+        <div className="flex-col justify-center">
+            <h1 className="text-6xl">Welcome to Rememzo</h1>
+            <p>This is a memory DLE game, where you can test your memory retention and eye for detail.</p>
+            <button className="mt-10"><Link to="/play">Play</Link></button>
+        </div>
     )
 }
