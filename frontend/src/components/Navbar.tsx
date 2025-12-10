4119ea2f728c1b4d48f2893ce439f20c7502c5cb
Karina Allard <karina.allard.92@gmail.com>
2025-12-10 13:26:26 +0100

    feat: create and add logo, refactor Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router"
+import logo from "../assets/logo-v3.png"
 
 export const Navbar = () => {
 
     return (
-        <header className="bg-gray-800 text-white p-4 flex justify-center">
+        <header className="p-4 flex justify-between px-10 items-center">
+            <img src={logo} alt="Rememzo" className="w-[300px]" />
             <nav>
-                <ul className="flex space-x-6">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/play">Play</Link></li>
-                <li><Link to="/login">Login</Link></li>
+                <ul className="flex gap-6 pt-3">
+                <li><Link to="/" className="hover:text-(--text-hover)">Home</Link></li>
+                <li><Link to="/play" className="hover:text-(--text-hover)">Play</Link></li>
+                <li><Link to="/login" className="hover:text-(--text-hover)">Login</Link></li>
                 </ul>
             </nav>
         </header>
