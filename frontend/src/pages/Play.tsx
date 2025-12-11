43f1153d8c663df1a54ad78ae88e63167ab2baf0
Karina Allard <karina.allard.92@gmail.com>
2025-12-11 13:51:50 +0100

    feat: add basic Play page with redirect to Question page

--- frontend/src/pages/Play.tsx
+++ frontend/src/pages/Play.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router"
+import { Button } from "../components/Button"
+
 export const Play = () => {
     return (
-        <>
-        
-        </>
+        <div className="w-full flex flex-col">
+            <h1 className="text-4xl text-(--text-hover) mb-6">Daily Puzzle #1</h1>
+            <p className="text-sm">2025-12-11</p>
+            <Link to="/play/question" className="block w-full mt-4">
+                <Button className="w-full">Start Game</Button>
+            </Link>
+        </div>
     )
 }
