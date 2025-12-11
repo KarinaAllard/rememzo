5ea299c9891e15243cb31b652be0443ca4dc9d9e
Karina Allard <karina.allard.92@gmail.com>
2025-12-11 12:59:24 +0100

    feat: refactor Login to use Input component

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -5,7 +5,7 @@ export const Layout = () => {
     return (
         <div className="flex flex-col min-h-screen min-w-full items-center">
             <Navbar />
-            <main className="flex grow mx-auto p-4">
+            <main className="flex grow mx-auto p-4 max-w-md w-full mt-12">
                 <Outlet />
             </main>
         </div>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,20 +1,23 @@
+import { Link } from "react-router"
 import { Button } from "../components/Button"
+import { Input } from "../components/Input"
 
 export const Login = () => {
     return (
-        <div className="">
-            <h1 className="text-4xl text-(--text) mb-6">Log in</h1>
-            <form className="flex flex-col">
-                <label className="mb-4">Email
-                    <br/>
-                    <input type="email" name="email" id="email" className="border border-(--text-hover)" />
-                </label>
-                <label className="mb-4">Password
-                    <br/>
-                <input type="password" name="password" id="password" className="border border-(--text-hover)" />
-                </label>
-                <Button>Log in</Button>
+        <div className="w-full flex flex-col">
+            <h1 className="text-4xl text-(--text-hover) mb-6">Log in</h1>
+            <p className="text-sm">To view your streak and other stats, log in here.</p>
+            <form className="flex flex-col mt-4">
+                <Input label="Email" type="email" name="email" />
+                <Input label="Password" type="password" name="password" />
+                <Button type="submit">Log in</Button>
             </form>
+            <div className="mt-4">
+                <p>Not registered yet? </p>
+                <p className="text-(--link) hover:text-(--link-hover) font-semibold">
+                    <Link to="/signup">Sign up here</Link>
+                </p>
+            </div>
         </div>
     )
 }
