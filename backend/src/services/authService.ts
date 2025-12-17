b08752cceebf8c3a1d421545662f8d91853ba8cf
Karina Allard <karina.allard.92@gmail.com>
2025-12-17 15:01:32 +0100

    chore: remove unused import

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User, { IUser } from "../models/User";
+import { IUser } from "../models/User";
 
 const SALT_ROUNDS = 10;
 const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
