d866f10f9ab7e5cd74be1229960efd16f4c5ef96
Karina Allard <karina.allard.92@gmail.com>
2025-12-18 11:47:39 +0100

    feat: implement Login logic to existing form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,18 +1,37 @@
-import { Link } from "react-router"
-import { Button } from "../components/Button"
-import { Input } from "../components/Input"
+import { Link, useNavigate } from "react-router";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+import { useState } from "react";
+import { login } from "../services/authService";
 
 export const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState<string | null>(null);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        try {
+            const result = await login(email, password);
+            localStorage.setItem("token", result.token);
+            navigate("/my-account");
+        } catch (error) {
+            console.error("Login failed:", error);
+            setError("Invalid email or password")
+        }
+    };
+
     return (
         <div className="w-full flex flex-col">
             <h1 className="text-4xl text-(--text-hover) mb-6">Log in</h1>
             <p className="text-sm">To view your streak and other stats, log in here.</p>
-            <form className="flex flex-col mt-4">
-                <Input label="Email" type="email" name="email" />
-                <Input label="Password" type="password" name="password" />
-                <Link to={"/my-account"} className="w-full">
-                    <Button type="submit" className="w-full">Log in</Button>
-                </Link>
+            <form onSubmit={handleSubmit} className="flex flex-col mt-4">
+                <Input label="Email" type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
+                <Input label="Password" type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+                {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+                <Button type="submit" className="w-full">Log in</Button>
             </form>
             <div className="mt-4">
                 <p>Not registered yet? </p>
