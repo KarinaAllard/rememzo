c3f397188443d4da40af63bb97324abc2d665043
Karina Allard <karina.allard.92@gmail.com>
2025-12-18 13:39:43 +0100

    feat: add rightIcon prop to Input

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -6,6 +6,8 @@ type InputProps = {
     id?: string;
     className?: string;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    rightIcon?: React.ReactNode;
+    onIconClick?: () => void;
 };
 
 export const Input = (props: InputProps) => {
@@ -15,19 +17,29 @@ export const Input = (props: InputProps) => {
                 {props.label}
             </label>
 
-            <input 
-                type={props.type || "text"} 
-                name={props.name}
-                value={props.value}
-                onChange={props.onChange}
-                id={props.id || props.name}
-                className={`
-                    mt-1 p-2 rounded-xs border border-(--text-hover)
-                    bg-(--background) text-(--text)
-                    focus:outline-none focus:border-(--cta)
-                    ${props.className || ""}    
-                `}
-            />
+            <div className="relative w-full">
+                <input 
+                    type={props.type || "text"} 
+                    name={props.name}
+                    value={props.value}
+                    onChange={props.onChange}
+                    id={props.id || props.name}
+                    className={`
+                        mt-1 p-2 pr-10 rounded-xs border border-(--text-hover)
+                        bg-(--background) text-(--text)
+                        focus:outline-none focus:border-(--cta)
+                        ${props.className || ""}    
+                    `}
+                />
+                {props.rightIcon && (
+                    <span
+                        className="absolute text-2xl right-2 top-1/2 -translate-y-1/2 cursor-pointer"
+                        onClick={props.onIconClick}
+                    >
+                        {props.rightIcon}
+                    </span>
+                )}
+            </div>
         </div>
     )
 }
