e233d4a86a0f47e361c721516e63fa524cabdf14
Karina Allard <karina.allard.92@gmail.com>
2025-12-18 13:46:41 +0100

    feat: add showPassword to SignUp

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,6 +3,7 @@ import { Button } from "../components/Button"
 import { Input } from "../components/Input"
 import { useState } from "react"
 import { register } from "../services/authService"
+import { LuEye, LuEyeClosed } from "../icons";
 
 export const Signup = () => {
     const [email, setEmail] = useState("");
@@ -10,6 +11,7 @@ export const Signup = () => {
     const [confirmPassword, setConfirmPassword] = useState("");
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate();
 
     const handleSubmit = async (e: React.FormEvent) => {
@@ -38,10 +40,36 @@ export const Signup = () => {
         <div className="w-full flex flex-col">
             <h1 className="text-4xl text-(--text-hover) mb-6">Sign Up</h1>
             <p className="text-sm">To keep track of your stats and streak, register here.</p>
-            <form onSubmit={handleSubmit} className="flex flex-col mt-4">
-                <Input label="Email" type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
-                <Input label="Password" type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
-                <Input label="Confirm Password" type="password" name="confirmPassword" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}/>
+            <form onSubmit={handleSubmit} className="flex flex-col mt-4 w-full">
+                <Input 
+                    label="Email" 
+                    type="email" 
+                    name="email" 
+                    value={email} 
+                    onChange={e => 
+                    setEmail(e.target.value)} 
+                    className="w-full"
+                />
+                <Input 
+                    label="Password" 
+                    type={showPassword ? "text" : "password"} 
+                    name="password" 
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)} 
+                    rightIcon={showPassword ? <LuEye /> : <LuEyeClosed />}
+                    onIconClick={() => setShowPassword(!showPassword)}
+                    className="w-full"
+                />
+                <Input 
+                    label="Confirm Password" 
+                    type={showPassword ? "text" : "password"}  
+                    name="confirmPassword" 
+                    value={confirmPassword} 
+                    onChange={e => setConfirmPassword(e.target.value)}
+                    rightIcon={showPassword ? <LuEye /> : <LuEyeClosed />}
+                    onIconClick={() => setShowPassword(!showPassword)}
+                    className="w-full"
+                />
                 {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
                 <Button type="submit" loading={loading}>Sign Up</Button>
             </form>
