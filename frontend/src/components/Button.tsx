1e6521dc07bd926a5610a2cd590fbbd87b5eceb0
Karina Allard <karina.allard.92@gmail.com>
2025-12-30 20:04:35 +0100

    feat: add additional colors for shine and success, apply styling to Button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -11,7 +11,7 @@ type ButtonProps = {
 const baseClasses = `
     relative px-4 py-2 cursor-pointer font-bold rounded-sm
     before:content-[''] before:absolute before:left-1/2 before:-translate-x-1/2 before:top-0
-    before:h-0.5 before:w-5/6
+    before:h-[2px] before:w-5/6
     after:content-[''] after:absolute after:left-1/2 after:-translate-x-1/2 after:bottom-0
     after:h-0.5 after:w-5/6
 `;
@@ -19,10 +19,10 @@ const baseClasses = `
 const variantClasses = {
 	primary: `
         bg-(--cta) text-white
-        before:bg-linear-to-r before:from-(--cta) before:via-(--text) before:to-(--cta) 
+        before:bg-linear-to-r before:from-(--cta) before:via-(--shine) before:to-(--cta) 
         after:bg-linear-to-r after:from-(--cta) after:via-(--dark-cta) after:to-(--cta)
         hover:text-white hover:bg-linear-to-b hover:from-(--cta) hover:to-(--cta-hover)
-        hover:before:from-(--cta) hover:before:via-(--text-hover) hover:before:to-(--cta) 
+        hover:before:from-(--cta) hover:before:via-(--shine) hover:before:to-(--cta) 
         hover:after:bg-linear-to-r hover:after:from-(--cta-hover) hover:after:via-(--dark-cta) hover:after:to-(--cta-hover)
     `,
 	secondary: `
