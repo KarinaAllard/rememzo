86015fae225c872b71961da6a160d0614c5e3756
Karina Allard <karina.allard.92@gmail.com>
2026-01-01 16:15:33 +0100

    feat: change font color to gray in MyAccount

--- frontend/src/pages/MyAccount.tsx
+++ frontend/src/pages/MyAccount.tsx
@@ -43,17 +43,17 @@ export const MyAccount = () => {
             {user ? (
                 <div className="flex flex-col gap-4 p-4 border border-neutral-800 mb-4">
                     <p>Email: 
-                        <span className="text-(--shine) font-semibold"> {user.email}</span>
+                        <span className="text-(--text-hover) font-semibold"> {user.email}</span>
                     </p>
                     <p className="flex gap-2">
                         Language preference: 
                         {user.preferences.language === "sv" ? <FlagSV /> : <FlagEN />}
-                        <span className="uppercase text-(--shine) font-semibold">
+                        <span className="uppercase text-(--text-hover) font-semibold">
                             {user.preferences.language}
                         </span>
                     </p>
                     <p>Current Streak: 
-                        <span className="text-(--shine) font-semibold"> {user.streak}</span>
+                        <span className="text-(--text-hover) font-semibold"> {user.streak}</span>
                     </p>
                 </div>
             ) : (
